c0eb1332e6a4ea2fca0bfc816f7f5f23b5a0a2eb
Doug James <vbdjames@gmail.com>
2019-10-14 08:42:45 -0400

    fixed typo

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :platform, PlatformWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
-config:platform, Platform.Repo,
+config :platform, Platform.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
